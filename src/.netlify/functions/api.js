069185266c430ac7088829b6e83c80a5c526d70f
Anthony Shew <anthonyshew@gmail.com>
2020-03-16 10:07:48 -0700

    Feature: Send an email through Sendgrid API.

--- src/.netlify/functions/api.js
+++ src/.netlify/functions/api.js
@@ -1,16 +1,38 @@
+require('dotenv').config()
 const express = require('express')
 const serverless = require('serverless-http')
-const bodyParser = require('body-parser')
+const sendGrid = require('@sendgrid/mail')
 
 const app = express()
 const router = express.Router()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 // API calls
 router.get('/hola', (req, res) => {
     res.send({ msg: 'Hello from a backend server' })
+})
+
+router.post('/mail', (req, res) => {
+    const emailMessage = {
+        to: process.env.AGENCY_EMAIL,
+        from: process.env.CLIENT_EMAIL,
+        subject: 'Message from MMSI!',
+        html: `<h1>It worked!</h1>
+    <p>A message.</p>
+    `,
+    }
+
+    sendGrid.send(emailMessage)
+
+    res.send({
+        statusCode: 200,
+        success: true,
+        errors: [],
+        data: null
+    })
 })
 
 if (process.env.ENVIRONMENT !== 'PRODUCTION') {
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,10 +18,19 @@ const BlogIndex = ({ data, location }) => {
       .then(res => setFoo(res.msg))
   }, [])
 
+  const handleClick = (e) => {
+    fetch('/.netlify/functions/api/mail', {
+      method: "POST",
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
+  }
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <p>{foo}</p>
+      <button onClick={handleClick}>Send out the email</button>
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
