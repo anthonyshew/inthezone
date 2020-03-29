bf22de43a533229de3325171d6f8a19874bb41ce
Anthony Shew <anthonyshew@gmail.com>
2020-03-28 17:59:33 -0700

    Install react-hook-form and create dear players page.

---
+++ src/pages/dear-players.js
@@ -0,0 +1,14 @@
+import React from "react"
+import '../styles/dear-players.scss'
+
+import Layout from "../components/layout"
+
+export default ({ location }) => {
+    return (
+        <Layout location={location}>
+            <div className="page-dear-players">
+                Pardon our dust! this page is still being constructed.
+            </div>
+        </Layout>
+    )
+}
